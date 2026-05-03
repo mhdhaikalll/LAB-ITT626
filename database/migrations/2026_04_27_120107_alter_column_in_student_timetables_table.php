453e60d94ad1abaf0864158f21a904172639036f
mhdhaikalll <atokzzz04@gmail.com>
2026-05-03 21:44:27 +0800

    init

---
+++ database/migrations/2026_04_27_120107_alter_column_in_student_timetables_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('student_timetables', function (Blueprint $table) {
+            $table->foreign('subject_id')->references('id')->on('subject');
+            $table->foreign('day_id')->references('id')->on('days');
+            $table->foreign('hall_id')->references('id')->on('halls');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('lecture_group_id')->references('id')->on('lecturer_groups');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('student_timetables', function (Blueprint $table) {
+            //
+        });
+    }
+};
