ba9212a929a5e9c1516f9c49bcdfd2a5ad9091f0
Haikal Iman <122687416+mhdhaikalll@users.noreply.github.com>
2026-05-31 22:53:19 +0800

    update: add variable to make editing easier

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -1,3 +1,8 @@
+<?php
+  const $name = "Haikal Iman"
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -188,7 +193,7 @@
           <img src="{{ asset('admin/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Haikal Iman</a>
+          <a href="#" class="d-block">{{ name }}</a>
         </div>
       </div>
 
